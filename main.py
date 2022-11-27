a4049745a87bb0aee47fa49ba024276b043a24f5
Nathan <nathan.mizzi.d57257@mcast.edu.mt>
2022-11-27 18:18:46 +0000

    Implemented Basic Error Based SQL Injection scan on the DVWA website (Hard coded login page value), links are taken from spider csv results.

--- main.py
+++ main.py
@@ -1,53 +1,29 @@
 # Imports
 
-import pyautogui
 import aspose.words as aw  # Used to create word document containing report
 from datetime import datetime
 from bs4 import BeautifulSoup as bs
 from urllib.parse import urljoin
 import requests
+import pandas as pd
 
 # Variable Declarations
-Thesis = True
 total_seconds = 0
 successful_hit_type = []
 hit_type = []
 
 # Contains all the Base URL's of the webpages to test on
-list_of_source_urls = ["http://127.0.0.1", "http://localhost:8080/WebGoat", "https://127.0.0.1/xvwa"]
+# list_of_source_csvs = ["BWAPP", "DVWA", "Mutillidae", "Orange_HRM", "Webgoat", "XVWA"]
+list_of_source_csvs = ["DVWA"]
 
-# DVWA Links
-
-root_url = "http://127.0.0.1/"
-website_url = "http://127.0.0.1"
-login_page = "http://127.0.0.1/login.php"
-sqli_page = "http://127.0.0.1/vulnerabilities/sqli/"
-
-# Mutillidae Links
-
-# root_url = "http://192.168.56.101"
-# website_url = "http://192.168.56.101/mutillidae"
-# login_page = "http://192.168.56.101/mutillidae/index.php?page=login.php"
-# sqli_page = "http://192.168.56.101/mutillidae/index.php?page=register.php"
+urls_to_test = []
 
 directoryListPath = "utils/directoryLists/dirbuster_200.txt"
 passwordListPath = "utils/passwordLists/passlist.txt"
 subdomainListPath = "utils/subdomainLists/subdomains.txt"
 usernameListPath = "utils/usernameLists/usernames_small.txt"
 
-listOfFunctionalities = ["Website Brute-Forcer", "Directory Brute-Forcer",
-                         "Sub Domain Brute-Forcer", "SQL Injection Scan", "Generate Report", "Exit"]
-
 # Utilities
-def webcrawler():
-
-    urls_to_crawl = []
-
-    for url in list_of_source_urls:
-        urls_to_crawl.append(automatedWebDirectoryFinder(url))
-
-    print("\n These URL's were obtained!")
-    print(urls_to_crawl)
 
 def request(url):
     try:
@@ -67,25 +43,7 @@ def differenceInSeconds(stardDate, endDate):
 
     return difference.total_seconds()
 
-def menuMaker():
-
-    try:
-        cnt = 0
-        for functionality in listOfFunctionalities:
-            cnt = cnt + 1
-            print(f"[{cnt}] {functionality}")
-
-        userInp = int(input("\nPlease Select The number of the functionality you would like to use: "))
-        userInp = userInp - 1
-
-        if userInp >= 0 and userInp <= len(listOfFunctionalities):
-            return listOfFunctionalities[userInp]
-        else:
-            return None
-    except Exception:
-        return None
-
-def generateReport():
+def generateReport(website_url):
 
     dateTimeToday = datetime.now().strftime('%d-%m-%Y_%H:%M:%S')
 
@@ -142,97 +100,20 @@ def generateReport():
 
     report.save(f"reports/report_{dateTimeToday}.docx")
 
-def displayMenuSelector():
-    while True:
+def readCsv():
 
-        methodChosen = menuMaker()
+    for file in list_of_source_csvs:
+        df = pd.read_csv('utils/websiteLinks/' + file + '.csv')
 
-        if methodChosen == "Website Brute-Forcer":
-
-            if "Website Brute-Forcer" not in hit_type:
-                hit_type.append("Website Brute-Forcer")
-
-            dateTimeStart = getCurrentDateTime()
-
-            if websiteBruteForce(login_page):
-
-                if "Website Log-in Brute Force" not in successful_hit_type:
-                    successful_hit_type.append("Website Log-in Brute Force")
-
-            dateTimeEnd = getCurrentDateTime()
-            total_seconds = total_seconds + differenceInSeconds(dateTimeStart, dateTimeEnd)
-
-        elif methodChosen == "Directory Brute-Forcer":
-
-            if "Directory Brute-Forcer" not in hit_type:
-                hit_type.append("Directory Brute-Forcer")
-
-            dateTimeStart = getCurrentDateTime()
-
-            if webDirectoryFinder(website_url):
-                if "Directory Brute Force" not in successful_hit_type:
-                    successful_hit_type.append("Directory Brute Force")
-
-            dateTimeEnd = getCurrentDateTime()
-            total_seconds = total_seconds + differenceInSeconds(dateTimeStart, dateTimeEnd)
-
-        elif methodChosen == "Sub Domain Brute-Forcer":
-
-            if "Sub Domain Brute-Forcer" not in hit_type:
-                hit_type.append("Sub Domain Brute-Forcer")
-
-            dateTimeStart = getCurrentDateTime()
-
-            if subDomainsFinder(website_url):
-                if "Sub Domain Brute Force" not in successful_hit_type:
-                    successful_hit_type.append("Sub Domain Brute Force")
-
-            dateTimeEnd = getCurrentDateTime()
-            total_seconds = total_seconds + differenceInSeconds(dateTimeStart, dateTimeEnd)
-
-        elif methodChosen == "SQL Injection Scan":
-
-            if "SQL Injection" not in hit_type:
-                hit_type.append("SQL Injection")
-
-            dateTimeStart = getCurrentDateTime()
-
-            with requests.Session() as s:
-
-                s.headers = {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:46.0) Gecko/20100101 Firefox/46.0',
-                    'Cookie': 'security=low; PHPSESSID=geo7gb3ehf5gfnbhrvuqu545i7'
-                }
-
-                resp = s.get(login_page)
-                parsed_html = bs(resp.content, features="html.parser")
-                input_value = parsed_html.body.find('input', attrs={'name': 'user_token'}).get("value")
-                data_dict = {"username": 'admin', "password": 'password', "Login": "Login",
-                             "user_token": input_value}
-
-                response = s.post(login_page, data_dict)
-
-                if sqlInjectionScan(sqli_page):
-                    if "SQL Injection" not in successful_hit_type:
-                        successful_hit_type.append("SQL Injection")
-
-            dateTimeEnd = getCurrentDateTime()
-            total_seconds = total_seconds + differenceInSeconds(dateTimeStart, dateTimeEnd)
-
-        elif methodChosen == "Generate Report":
-            generateReport()
-
-        elif methodChosen == "Exit":
-            exit()
-
-        elif methodChosen is None:
-            print("An Invalid Option Was Inputted, Please Try Again")
-
-        input("\nPress Enter To Continue...")
-        pyautogui.hotkey('shift', 'l')  # Clears the pycharm console, will not work unless Clear All hotkey is set
+        for index, row in df.iterrows():
+            try:
+                if df.iloc[index]["Processed"] == True:
+                    # print(df.iloc[index]["URI"])
+                    urls_to_test.append(df.iloc[index]["URI"])
+            except Exception as e:
+                print(e)
 
 # Brute Forcers
-
 def websiteBruteForce(page_url):
 
     with open(usernameListPath, "r") as usernames:
@@ -266,130 +147,6 @@ def websiteBruteForce(page_url):
 
         print("\nUsername and password are not in wordlists.")
         return False
-
-# Finders
-
-def webDirectoryFinder(page_url):
-
-    dirFound = False
-
-    target_url = page_url
-
-    print("\nBeginning Web Directory Brute-Force...\n")
-
-    file = open(directoryListPath,"r")
-
-    for line in file:
-        word = line.strip()
-        full_url = target_url + "/" + word
-        response = request(full_url)
-
-        if response:
-            print("[+] Discovered Directory At This Link: " + full_url)
-            dirFound = True
-
-    return  dirFound
-
-def automatedWebDirectoryFinder(page_url):
-
-    list_of_directories = []
-
-    target_url = page_url
-
-    print("\nBeginning Web Directory Brute-Force...\n")
-
-    file = open(directoryListPath, "r")
-
-    list_of_attempted_urls = []
-
-    for line in file:
-        word = line.strip()
-        full_url = target_url + "/" + word
-        list_of_attempted_urls.append(full_url)
-        response = request(full_url)
-
-        if response:
-
-            if response.history:
-
-                directory_found = response.url.split(target_url + "/")
-
-                print("Request was redirected")
-
-                # Final Destination -
-                print(response.status_code, response.url)
-
-                if word != directory_found[1].strip("/"):
-                    print("Redirect led to login page, ignoring...")
-                else:
-                    print("[+] Discovered Directory At This Link: " + full_url)
-                    list_of_directories.append(full_url)
-            else:
-                print("[+] Discovered Directory At This Link: " + full_url)
-                list_of_directories.append(full_url)
-
-        else:
-            print("[-] Failed To Find Directory At This Link: " + full_url)
-
-    for subdirectory in list_of_attempted_urls:
-
-        file = open(directoryListPath, "r")
-
-        for line in file:
-            word = line.strip()
-            full_url = subdirectory + "/" + word
-            response = request(full_url)
-
-            if response:
-
-                if response.history:
-
-                    directory_found = response.url.split(target_url + "/")
-
-                    print("Request was redirected")
-
-                    # Final Destination -
-                    print(response.status_code, response.url)
-
-                    if word not in subdirectory:
-                        if word != directory_found[1].strip("/"):
-                            print("Redirect led to login page, ignoring...")
-                        else:
-                            print("[+] Discovered Directory At This Link: " + full_url)
-                            list_of_directories.append(full_url)
-                    else:
-                        print("Skipped as Duplicate..")
-                else:
-                    print("[+] Discovered Directory At This Link: " + full_url)
-                    list_of_directories.append(full_url)
-
-            else:
-                print("[-] Failed To Find Directory At This Link: " + full_url)
-
-    return list_of_directories
-
-def subDomainsFinder(page_url):
-
-    subDomainFound = False
-
-    target_url = page_url
-    target_url = target_url.replace("http://", "")
-
-    print("\nBeginning Sub Domain Brute-Force...\n")
-
-    file = open(subdomainListPath,"r")
-
-    for line in file:
-        word = line.strip()
-        full_url = word + "." + target_url
-        response = request("http://" + full_url)
-
-        if response:
-            print("[+] Discovered Subdomain At This Link: " + full_url)
-            subDomainFound = True
-
-    return subDomainFound
-
 
 # SQL Injection
 
@@ -481,92 +238,36 @@ def sqlInjectionScan(url):
 
 if __name__ == '__main__':
 
-    if not Thesis:
-        while True:
-            methodChosen = menuMaker()
+        # Populate array of urls from csv files
+        readCsv()
 
-            if methodChosen == "Website Brute-Forcer":
+        # Test urls accordingly
+        with requests.Session() as s:
 
-                if "Website Brute-Forcer" not in hit_type:
-                    hit_type.append("Website Brute-Forcer")
+            try:
 
-                dateTimeStart = getCurrentDateTime()
+                s.headers = {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:46.0) Gecko/20100101 Firefox/46.0',
+                    'Cookie': 'security=low; PHPSESSID=geo7gb3ehf5gfnbhrvuqu545i7'
+                }
 
-                if websiteBruteForce(login_page):
+                resp = s.get('http://127.0.0.1/login.php')
+                parsed_html = bs(resp.content, features="html.parser")
+                input_value = parsed_html.body.find('input', attrs={'name': 'user_token'}).get("value")
+                data_dict = {"username": 'admin', "password": 'password', "Login": "Login",
+                             "user_token": input_value}
 
-                    if "Website Log-in Brute Force" not in successful_hit_type:
-                        successful_hit_type.append("Website Log-in Brute Force")
+                response = s.post('http://127.0.0.1/login.php', data_dict)
 
-                dateTimeEnd = getCurrentDateTime()
-                total_seconds = total_seconds + differenceInSeconds(dateTimeStart, dateTimeEnd)
+                print(response.content)
 
-            elif methodChosen == "Directory Brute-Forcer":
+                for url in urls_to_test:
 
-                if "Directory Brute-Forcer" not in hit_type:
-                    hit_type.append("Directory Brute-Forcer")
+                    print(url)
 
-                dateTimeStart = getCurrentDateTime()
-
-                if webDirectoryFinder(website_url):
-                    if "Directory Brute Force" not in successful_hit_type:
-                        successful_hit_type.append("Directory Brute Force")
-
-                dateTimeEnd = getCurrentDateTime()
-                total_seconds = total_seconds + differenceInSeconds(dateTimeStart, dateTimeEnd)
-
-            elif methodChosen == "Sub Domain Brute-Forcer":
-
-                if "Sub Domain Brute-Forcer" not in hit_type:
-                    hit_type.append("Sub Domain Brute-Forcer")
-
-                dateTimeStart = getCurrentDateTime()
-
-                if subDomainsFinder(website_url):
-                    if "Sub Domain Brute Force" not in successful_hit_type:
-                        successful_hit_type.append("Sub Domain Brute Force")
-
-                dateTimeEnd = getCurrentDateTime()
-                total_seconds = total_seconds + differenceInSeconds(dateTimeStart, dateTimeEnd)
-
-            elif methodChosen == "SQL Injection Scan":
-
-                if "SQL Injection" not in hit_type:
-                    hit_type.append("SQL Injection")
-
-                dateTimeStart = getCurrentDateTime()
-
-                with requests.Session() as s:
-
-                    s.headers = {
-                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:46.0) Gecko/20100101 Firefox/46.0',
-                        'Cookie': 'security=low; PHPSESSID=geo7gb3ehf5gfnbhrvuqu545i7'
-                    }
-
-                    resp = s.get(login_page)
-                    parsed_html = bs(resp.content, features="html.parser")
-                    input_value = parsed_html.body.find('input', attrs={'name': 'user_token'}).get("value")
-                    data_dict = {"username": 'admin', "password": 'password', "Login": "Login",
-                                 "user_token": input_value}
-
-                    response = s.post(login_page, data_dict)
-
-                    if sqlInjectionScan(sqli_page):
+                    if sqlInjectionScan(url):
                         if "SQL Injection" not in successful_hit_type:
                             successful_hit_type.append("SQL Injection")
 
-                dateTimeEnd = getCurrentDateTime()
-                total_seconds = total_seconds + differenceInSeconds(dateTimeStart, dateTimeEnd)
-
-            elif methodChosen == "Generate Report":
-                generateReport()
-
-            elif methodChosen == "Exit":
-                exit()
-
-            elif methodChosen is None:
-                print("An Invalid Option Was Inputted, Please Try Again")
-
-            input("\nPress Enter To Continue...")
-            pyautogui.hotkey('shift', 'l')  # Clears the pycharm console, will not work unless Clear All hotkey is set
-    else:
-        webcrawler()
+            except Exception as e:
+                pass
