c9a3ea5ee039c39ff379cf380ef563ce9606d6eb
Nathan <nathan.mizzi.d57257@mcast.edu.mt>
2023-03-23 19:12:54 +0000

    Added Support for both The Juice Shop webpage and the BodgeIt webpage, this includes both blind and error based injection. Also made it so that if a page contains no forms the page is skipped and not attempted several times in error based. Also did more bug fixing, mostly related to errors in report generation.

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ hit_type["XVWA"] = []
 hit_type["Orange_HRM"] = []
 hit_type["Mutillidae"] = []
 hit_type["Webgoat"] = []
+hit_type["Juice_Shop"] = []
+hit_type["Bodgeit"] = []
 
 # A Dictionary containing dataframes which contain info regarding a website
 reportDetails = {}
@@ -36,7 +38,7 @@ reportDetails = {}
 
 # It is important that the names of the sites below match the respective csv files name perfectly.
 # list_of_source_csvs = ["BWAPP", "DVWA", "Mutillidae", "Orange_HRM", "Webgoat", "XVWA"]
-list_of_source_csvs = ["DVWA", "XVWA", "Orange_HRM", "Mutillidae", "Webgoat"]
+list_of_source_csvs = ["Bodgeit"]
 
 urls_to_test = {}
 vulnerable_urls = []
@@ -371,10 +373,15 @@ def sqlInjectionScan(url, cookies, nameOfWebsite):
 
         if url not in tested_urls:
 
+            print("TESTING URL: " + url)
+
             forms = get_all_forms(url, cookies)
 
             if len(forms) >= 1:
                 print(f"[+] Detected {len(forms)} forms on {url}.")
+            else:
+                tested_urls.append(url)
+                return False
 
             for form in forms:
                 form_details = get_form_details(form)
@@ -424,64 +431,73 @@ def sqlInjectionScan(url, cookies, nameOfWebsite):
 
 def blind_sql(url, cookies):
 
-    setSleepTime = 2
+    print("TESTING URL: " + url)
 
-    # Steps
+    try:
 
-    # Get Form Details
-    forms = get_all_forms(url, cookies)
+        setSleepTime = 2
 
-    if len(forms) >= 1:
-        print(f"[+] Detected {len(forms)} forms on {url}.")
+        # Steps
 
-    for form in forms:
-        form_details = get_form_details(form)
+        # Get Form Details
+        forms = get_all_forms(url, cookies)
 
-        try:
-            if "xvwa" in url and form_details["inputs"][0]["name"] == "username":  # Adding condition to skip login form for XVWA as it was causing issues
+        if len(forms) >= 1:
+            print(f"[+] Detected {len(forms)} forms on {url}.")
+
+        for form in forms:
+            form_details = get_form_details(form)
+
+            try:
+                if "xvwa" in url and form_details["inputs"][0]["name"] == "username":  # Adding condition to skip login form for XVWA as it was causing issues
+                    continue
+            except:
                 continue
-        except:
-            continue
 
-        for c in "\"'":
+            for c in "\"'":
 
-            data = {}
-            for input_tag in form_details["inputs"]:
-                if input_tag["type"] == "hidden" or input_tag["value"]:
-                    try:
-                        data[input_tag["name"]] = input_tag["value"] + c
-                    except:
-                        pass
-                elif input_tag["type"] != "submit":
-                    # Inject SQL and sleep command
-                    data[input_tag["name"]] = f"SLEEP( " + str(setSleepTime) +")/*' || SLEEP(" + str(setSleepTime) + ") || '\" || SLEEP(" + str(setSleepTime) + ") || \"*/"
+                data = {}
+                for input_tag in form_details["inputs"]:
+                    if input_tag["type"] == "hidden" or input_tag["value"]:
+                        try:
+                            data[input_tag["name"]] = input_tag["value"] + c
+                        except:
+                            pass
+                    elif input_tag["type"] != "submit":
+                        # Inject SQL and sleep command
+                        data[input_tag["name"]] = f"SLEEP( " + str(setSleepTime) +")/*' || SLEEP(" + str(setSleepTime) + ") || '\" || SLEEP(" + str(setSleepTime) + ") || \"*/"
 
-            url = urljoin(url, form_details["action"])
+                url = urljoin(url, form_details["action"])
 
-            # Get Response
-            if form_details["method"] == "post":
-                s.cookies.clear()
-                res = s.post(url, data=data)
-            elif form_details["method"] == "get":
-                s.cookies.clear()
-                res = s.get(url, params=data)
+                # Get Response
+                if form_details["method"] == "post":
+                    s.cookies.clear()
+                    res = s.post(url, data=data)
+                elif form_details["method"] == "get":
+                    s.cookies.clear()
+                    res = s.get(url, params=data)
 
-            requestTime = res.elapsed.total_seconds()
-            tested_urls.append(url)
+                requestTime = res.elapsed.total_seconds()
+                tested_urls.append(url)
 
-            # If time to respond is greater than the sleep command, assume that blind SQL Injection is possible
-            if requestTime >= setSleepTime:
+                # If time to respond is greater than the sleep command, assume that blind SQL Injection is possible
+                if requestTime >= setSleepTime:
 
-                print("[+] BLind SQL Injection vulnerability detected, link:", url)
-                print("[+] Form:")
-                print(form_details)
+                    print("[+] BLind SQL Injection vulnerability detected, link:", url)
+                    print("[+] Form:")
+                    print(form_details)
 
-                return True
-            # Else, command ignored, SQL Injection failed
-            else:
-                pass
+                    return True
+                # Else, command ignored, SQL Injection failed
+                else:
+                    pass
 
-    return False
+        return False
+
+    except:
+        print("Error analysing URL: " + url)
+        # traceback.print_exc()
+        return False
 
 # Website authenticators
 
@@ -916,6 +932,178 @@ def WebGoat_Blind(urls):
         print(e)
         traceback.print_exc()
 
+def Juice_Shop_error_based(urls):
+
+    total_seconds["Juice_Shop"] = 0
+    timeStarted = getCurrentDateTime()
+    sqliStringsPerWebsite["Juice_Shop"] = []
+
+    try:
+
+        sqliStringsAttemptedInTotal["Juice_Shop"] = 0
+        successful_hit_type["Juice_Shop"] = []
+        vulnerableWebPagesInSite["Juice_Shop"] = []
+        safeWebPagesInSite["Juice_Shop"] = []
+        hit_type["Juice_Shop"].append("Error Based SQL Injection")
+
+        # Firstly, create a logged-in session in order to create requests
+        resp = s.get('http://localhost:3000/#/login')
+        parsed_html = bs(resp.content, features="html.parser")
+        data_dict = {"email": 'test@email.com', "password": 'password!', "Submit": "loginButton"}
+
+        response = s.post("http://localhost:3000/#/login", data_dict)
+        cookies = response.cookies
+        response_content = response.content.decode()
+
+        # Then test other urls
+
+        for url in urls["Juice_Shop"]:
+
+            try:
+
+                if sqlInjectionScan(url, cookies, "Juice_Shop"):
+
+                    if url not in vulnerable_urls:
+                        vulnerable_urls.append("Juice_Shop: " + url)
+
+                    vulnerableWebPagesInSite["Juice_Shop"].append(url)
+
+                    if "SQL Injection" not in successful_hit_type["Juice_Shop"]:
+                        successful_hit_type["Juice_Shop"].append("SQL Injection")
+
+                else:
+                    safeWebPagesInSite["Juice_Shop"].append(url)
+
+            except:
+                continue
+
+    except Exception as e:
+        print("\nJuice_Shop Error: \n")
+        print(e)
+        traceback.print_exc()
+
+    timeEnded = getCurrentDateTime()
+    total_seconds["Juice_Shop"] += differenceInSeconds(timeStarted, timeEnded)
+
+def Juice_Shop_Blind(urls):
+
+    print("\n --- BLIND SQL --- \n")
+
+    total_seconds["Juice_Shop"] = 0
+    timeStarted = getCurrentDateTime()
+    sqliStringsPerWebsite["Juice_Shop"] = []
+
+    try:
+
+        sqliStringsAttemptedInTotal["Juice_Shop"] = 0
+        successful_hit_type["Juice_Shop"] = []
+        vulnerableWebPagesInSite["Juice_Shop"] = []
+        safeWebPagesInSite["Juice_Shop"] = []
+        hit_type["Juice_Shop"].append("BLind SQL Injection")
+
+        # Firstly, create a logged-in session in order to create requests
+        resp = s.get('http://localhost:3000/#/login')
+        parsed_html = bs(resp.content, features="html.parser")
+        data_dict = {"email": 'test@email.com', "password": 'password!', "Submit": "loginButton"}
+
+        response = s.post("http://localhost:3000/#/login", data_dict)
+        cookies = response.cookies
+        response_content = response.content.decode()
+
+        for url in urls["Juice_Shop"]:
+
+            if blind_sql(url, cookies):
+
+                vulnerableWebPagesInSite["Juice_Shop"].append(url)
+
+                vulnerable_urls.append("Juice_Shop: " + url)
+
+                if "Blind SQL Injection" not in successful_hit_type["Juice_Shop"]:
+                    successful_hit_type["Juice_Shop"].append("Blind SQL Injection")
+            else:
+                safeWebPagesInSite["Juice_Shop"].append(url)
+
+        timeEnded = getCurrentDateTime()
+        total_seconds["Juice_Shop"] += differenceInSeconds(timeStarted, timeEnded)
+
+    except Exception as e:
+        print("\nBlind Juice_Shop Error: \n")
+        print(e)
+        traceback.print_exc()
+
+def Bodgeit_error_based(urls):
+
+    print("\n --- Error Based SQL --- \n")
+
+    total_seconds["Bodgeit"] = 0
+    timeStarted = getCurrentDateTime()
+    sqliStringsPerWebsite["Bodgeit"] = []
+
+    try:
+        sqliStringsAttemptedInTotal["Bodgeit"] = 0
+        successful_hit_type["Bodgeit"] = []
+        vulnerableWebPagesInSite["Bodgeit"] = []
+        safeWebPagesInSite["Bodgeit"] = []
+        hit_type["Bodgeit"].append("Error Based SQL Injection")
+
+
+        # Then test other urls
+
+        for url in urls["Bodgeit"]:
+
+            if sqlInjectionScan(url, None, "Bodgeit"):
+
+                vulnerableWebPagesInSite["Bodgeit"].append(url)
+
+                vulnerable_urls.append("Bodgeit: " + url)
+
+                if "Error Based SQL Injection" not in successful_hit_type["Bodgeit"]:
+                    successful_hit_type["Bodgeit"].append("Error Based SQL Injection")
+            else:
+                safeWebPagesInSite["Bodgeit"].append(url)
+    except Exception as e:
+        print("\nBodgeit Error: \n")
+        print(e)
+        traceback.print_exc()
+
+    # Get the current time when all tests end
+    timeEnded = getCurrentDateTime()
+    total_seconds["Bodgeit"] += differenceInSeconds(timeStarted, timeEnded)
+
+def Bodgeit_Blind(urls):
+    print("\n --- BLIND SQL --- \n")
+
+    total_seconds["Bodgeit"] = 0
+    timeStarted = getCurrentDateTime()
+    sqliStringsPerWebsite["Bodgeit"] = []
+
+    try:
+
+        sqliStringsAttemptedInTotal["Bodgeit"] = 0
+        successful_hit_type["Bodgeit"] = []
+        vulnerableWebPagesInSite["Bodgeit"] = []
+        safeWebPagesInSite["Bodgeit"] = []
+        hit_type["Bodgeit"].append("BLind SQL Injection")
+
+        for url in urls["Bodgeit"]:
+
+            if blind_sql(url, None):
+
+                vulnerableWebPagesInSite["Bodgeit"].append(url)
+                vulnerable_urls.append("Bodgeit: " + url)
+
+                if "Blind SQL Injection" not in successful_hit_type["Bodgeit"]:
+                    successful_hit_type["Bodgeit"].append("Blind SQL Injection")
+            else:
+                safeWebPagesInSite["Bodgeit"].append(url)
+
+        timeEnded = getCurrentDateTime()
+        total_seconds["Bodgeit"] += differenceInSeconds(timeStarted, timeEnded)
+
+    except Exception as e:
+        print("\nBlind Bodgeit Error: \n")
+        print(e)
+        traceback.print_exc()
 
 if __name__ == '__main__':
 
@@ -960,13 +1148,20 @@ if __name__ == '__main__':
             #     WebGoat_error_based(urls_to_test)
                 WebGoat_Blind(urls_to_test)
 
-        # TODO: Implement Juice Shop
+        if "Juice_Shop" in list_of_source_csvs:
+            with requests.Session() as s:
+                # Juice_Shop_error_based(urls_to_test)
+                Juice_Shop_Blind(urls_to_test)
 
         # TODO: Implement Moodle
 
         # TODO: Implement bWAPP
 
-        # TODO: Implement BodgeIt
+        # TODO: Investigate Lack of investigated links and failed detections in Bodgeit
+        if "Bodgeit" in list_of_source_csvs:
+            with requests.Session() as s:
+                Bodgeit_error_based(urls_to_test)
+                Bodgeit_Blind(urls_to_test)
 
         # TODO: Implement WackoPicko
 
